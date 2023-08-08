aba97541af219d565d63014dfb80b07962226afc
Francisco <gaviria.reyes@gmail.com>
2023-08-07 19:40:20 -0500

    adding creating roles function and modifying routes

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,16 @@ import express, { Application } from "express";
 import authRoutes from "./routes/auth.routes";
 import productsRoutes from "./routes/products.routes";
 import morgan from "morgan";
+import { createRoles } from "./libs/initialSetup";
 
 const app: Application = express();
+createRoles();//Create Roles in database
 app.use(morgan('dev')); //log for request
 app.use(express.json());
 app.set("port", 4000);
 
 //routes
-app.use(authRoutes);
-app.use('/products', productsRoutes);
+app.use('/api/products', productsRoutes);
+app.use('/api/auth', authRoutes);
 
 export default app; 
